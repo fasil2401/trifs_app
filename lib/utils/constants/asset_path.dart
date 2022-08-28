87c818983374827de7cc16f4a32ed56799793686
Salman Salim <nssalman97@gmail.com>
2022-08-28 12:44:04 +0530

    initial commit

---
+++ lib/utils/constants/asset_path.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String base = 'assets/images/';
+  static const String logo = '${base}logo.png';
+}
+
+class AppIcons {
+  static const String base = 'assets/icons/';
+  static const String logo = '${base}home.svg';  
+}
