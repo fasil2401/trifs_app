658005e5b97f780bc708239526f0f676b31d4721
Salman Salim <nssalman97@gmail.com>
2022-09-28 23:02:43 +0530

    main screens done

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryColor = Color(0xFF00A6F6);
+  static const Color secondaryColor = Color.fromARGB(255, 0, 130, 195);
   static const Color scaffoldBackground = Color(0xFFF9F9F9);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
   static const Color grey = Color(0xFFA9A9A9);
   static const Color lightGrey = Color(0xFFE5E5E5);
   static const Color yellow = Color(0xFFF6B100);
+  static const Color red = Color(0xFFE60000);
 }
--- lib/view/Profile Screen/profile_screen.dart
+++ lib/view/Profile Screen/profile_screen.dart
@@ -1,10 +1,231 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:trifs_app/utils/constants/asset_path.dart';
+import 'package:trifs_app/utils/constants/colors.dart';
+import 'package:trifs_app/view/Components/custom_text.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 20.h,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 8.h,
+                      ),
+                      Text(
+                        'Trifs User',
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          color: AppColors.secondaryColor,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0.5.h,
+                      ),
+                      Text(
+                        '9876543210',
+                        style: TextStyle(
+                          fontSize: 10.sp,
+                          color: AppColors.grey,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2.h,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomText.buildTitleText(
+                                title: 'Dashboard',
+                              ),
+                              SizedBox(
+                                height: 1.h,
+                              ),
+                              _buildAccountRows(
+                                title: 'About Us',
+                                icon: Icons.home,
+                              ),
+                              SizedBox(
+                                height: 2.5.h,
+                              ),
+                              _buildAccountRows(
+                                title: 'Notifications',
+                                icon: Icons.notifications,
+                              ),
+                              SizedBox(
+                                height: 2.5.h,
+                              ),
+                              _buildAccountRows(
+                                title: 'Privacy',
+                                icon: Icons.privacy_tip,
+                              ),
+                              SizedBox(
+                                height: 2.5.h,
+                              ),
+                              _buildAccountRows(
+                                title: 'Terms & Conditions',
+                                icon: Icons.flag_outlined,
+                              ),
+                              SizedBox(
+                                height: 2.5.h,
+                              ),
+                              _buildAccountRows(
+                                title: 'Support',
+                                icon: Icons.support_agent,
+                              ),
+                              SizedBox(
+                                height: 4.h,
+                              ),
+                              _buildText(
+                                title: 'My Account',
+                                color: AppColors.grey,
+                              ),
+                              SizedBox(
+                                height: 3.5.h,
+                              ),
+                              _buildText(
+                                title: 'Change Password',
+                                color: AppColors.primaryColor,
+                              ),
+                              SizedBox(
+                                height: 2.h,
+                              ),
+                              _buildText(
+                                title: 'Logout',
+                                color: AppColors.red,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 10.h,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.black,
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 2.h,
+                ),
+                CircleAvatar(
+                  radius: 6.h,
+                  backgroundColor: AppColors.primaryColor,
+                  child: CircleAvatar(
+                    radius: 5.8.h,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage(
+                      AppImages.alappuzha,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Text _buildText({
+    required String title,
+    required Color color,
+  }) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 12.sp,
+        color: color,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Row _buildAccountRows({
+    required String title,
+    required IconData icon,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 16,
+          backgroundColor: AppColors.primaryColor,
+          child: Icon(
+            icon,
+            color: AppColors.white,
+            size: 18,
+          ),
+        ),
+        SizedBox(
+          width: 4.w,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 14.sp,
+            color: AppColors.black,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: ClipOval(
+              child: Container(
+                height: 4.h,
+                width: 4.h,
+                color: AppColors.secondaryColor,
+                child: Icon(
+                  Icons.arrow_forward_ios,
+                  color: AppColors.white,
+                  size: 10,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
